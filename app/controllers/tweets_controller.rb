ee6af48b90a2a44df051f7cbb509e48459cb6aae
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-02-01 09:01:34 +0900

    before_actionビフォーアクションメソッドによるコード省略

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,7 +19,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -26,7 +27,6 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
@@ -34,4 +34,7 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:name, :image, :text)
   end
   
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 end
